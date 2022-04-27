fe1810487a285b8533fd29fcbfc5425e8ddbafd3
Tuval Gutman <tuval.gutman@gmail.com>
2022-04-27 16:21:26 +0300

    devide

--- Board.js
+++ Board.js
@@ -1,37 +1,76 @@
 class Board {
-    board = [];
-    selectedCell = undefined;
-  
-    constructor() { }
-  
-    getCell = (rowIndex, columnIndex) => {
-      return this.board[rowIndex][columnIndex];
-    };
-    movePiece = (cell) => {
-      if (!cell.isEmpty()) {
-        if (cell.isOtherPlayerCell) {
-          if (cell.piece.name == PIECES.KING) {
-            if (confirm("CheckMate, do you want to restart?") == true) {
-              location.reload();
-            }
-            else {
-              window.close();
-            }
+  board = [];
+  selectedCell = undefined;
+
+  constructor() { }
+
+  isFirstPlayerTurn = () => {
+    if (countSteps % 2 == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  isSecondPlayerTurn = () => {
+    if (countSteps % 2 != 0) {
+      return true;
+    }
+    return false;
+  }
+
+  isAbleToMove = (cell) => {
+    if (this.isFirstPlayerTurn() && cell.piece.color == FIRST_PLAYER_COLOR ||
+      this.isSecondPlayerTurn() && cell.piece.color == SECOND_PLAYER_COLOR) {
+      return true;
+    }
+    cell.isAvailable = false;
+    cell.updateAvailable();
+    alert("not your turn");
+    return false;
+  }
+
+  getCell = (rowIndex, columnIndex) => {
+    return this.board[rowIndex][columnIndex];
+  };
+  movePiece = (cell) => {
+    if (!cell.isEmpty()) {
+      if (cell.isOtherPlayerCell) {
+        if (cell.piece.name == PIECES.KING) {
+          if (confirm("CheckMate, do you want to restart?") == true) {
+            location.reload();
           }
-          cell.htmlElement.removeChild(cell.piece.htmlElement);
-          cell.piece = undefined;
+          else {
+            window.close();
+          }
+        }
+        cell.htmlElement.removeChild(cell.piece.htmlElement);
+        cell.piece = undefined;
+      }
+    }
+    const selectedImg = this.selectedCell.piece.htmlElement;
+    cell.isAVAILABLE = false;
+    cell.piece = Object.assign(this.selectedCell.piece);
+    cell.htmlElement.appendChild(selectedImg);
+    this.selectedCell.piece = undefined;
+    countSteps++;
+    if (this.kingIsCheck(cell.piece.getMoves(cell.rowIndex, cell.columnIndex, this))) {
+      alert("CHECK");
+    }
+  };
+
+  kingIsCheck = (moves) => {
+    for (let index = 0; index < moves.length; index++) {
+      if (!moves[index].isEmpty()) {
+        if (moves[index].piece.name == PIECES.KING) {
+          return true;
         }
       }
-      const selectedImg = this.selectedCell.piece.htmlElement;
-      cell.isAVAILABLE = false;
-      cell.piece = Object.assign(this.selectedCell.piece);
-      cell.htmlElement.appendChild(selectedImg);
-      this.selectedCell.piece = undefined;
-    };
-  
-  
-    onCellClicked = (cell) => {
-      if (!cell.isAvailable) {
+    }
+    return false;
+  }
+  onCellClicked = (cell) => {
+    if (!cell.isAvailable) {
+      if (this.isAbleToMove(cell)) {
         if (this.selectedCell) {
           //if its not first time
           if (this.selectedCell !== cell) {
@@ -65,7 +104,6 @@ class Board {
           }
         } else if (!cell.isEmpty()) {
           // if its first time
-  
           this.selectedCell = cell;
           const moves = this.selectedCell.piece.getMoves(
             this.selectedCell.rowIndex,
@@ -78,59 +116,59 @@ class Board {
           relatArray = [...moves];
         }
       }
-      else {
-        this.movePiece(cell);
-        this.updateToUnAvailible(relatArray);
-        this.selectedCell.isSelected = false;
-        this.selectedCell.updateSelected();
-        this.selectedCell = undefined;
-      }
-      this.colorAllCells(relatArray);
+    } else {
+      this.movePiece(cell);
+      this.updateToUnAvailible(relatArray);
+      this.selectedCell.isSelected = false;
+      this.selectedCell.updateSelected();
+      this.selectedCell = undefined;
     }
-  
-    updateToAvailible(moves) {
-      for (let index = 0; index < moves.length; index++) {
-        moves[index].isAvailable = true;
+    this.colorAllCells(relatArray);
+  }
+
+  updateToAvailible(moves) {
+    for (let index = 0; index < moves.length; index++) {
+      moves[index].isAvailable = true;
+    }
+  };
+
+  updateToUnAvailible(moves) {
+    for (let index = 0; index < moves.length; index++) {
+      moves[index].isAvailable = false;
+    }
+  };
+
+  colorAllCells(moves) {
+    for (let index = 0; index < moves.length; index++) {
+      moves[index].updateAvailable();
+    }
+  };
+
+
+  createBoard() {
+    let table = document.createElement("table");
+    let tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+
+    for (let rowIndex = 0; rowIndex < ROW_SIZE; rowIndex++) {
+      let htmlTr = document.createElement("tr");
+      const boardRowArray = [];
+      for (let columnIndex = 0; columnIndex < ROW_SIZE; columnIndex++) {
+        const pieceFactory = PieceFactory();
+        const piece = pieceFactory.createPiece(rowIndex, columnIndex);
+        const cell = new Cell(
+          rowIndex,
+          columnIndex,
+          false,
+          piece,
+          this.onCellClicked
+        );
+        boardRowArray.push(cell);
+        htmlTr.appendChild(cell.htmlElement);
       }
-    };
-  
-    updateToUnAvailible(moves) {
-      for (let index = 0; index < moves.length; index++) {
-        moves[index].isAvailable = false;
-      }
-    };
-  
-    colorAllCells(moves) {
-      for (let index = 0; index < moves.length; index++) {
-        moves[index].updateAvailable();
-      }
-    };
-  
-  
-    createBoard() {
-      let table = document.createElement("table");
-      let tbody = document.createElement("tbody");
-      table.appendChild(tbody);
-      document.body.appendChild(table);
-  
-      for (let rowIndex = 0; rowIndex < ROW_SIZE; rowIndex++) {
-        let htmlTr = document.createElement("tr");
-        const boardRowArray = [];
-        for (let columnIndex = 0; columnIndex < ROW_SIZE; columnIndex++) {
-          const pieceFactory = PieceFactory();
-          const piece = pieceFactory.createPiece(rowIndex, columnIndex);
-          const cell = new Cell(
-            rowIndex,
-            columnIndex,
-            false,
-            piece,
-            this.onCellClicked
-          );
-          boardRowArray.push(cell);
-          htmlTr.appendChild(cell.htmlElement);
-        }
-        this.board.push(boardRowArray);
-        tbody.appendChild(htmlTr);
-      }
+      this.board.push(boardRowArray);
+      tbody.appendChild(htmlTr);
     }
   }
+}
--- Cell.js
+++ Cell.js
@@ -1,76 +1,76 @@
 class Cell {
-    constructor(rowIndex, columnIndex, isAvailable, piece, onCellClicked) {
-      this.rowIndex = rowIndex;
-      this.columnIndex = columnIndex;
-      this.isAvailable = isAvailable;
-      this.onCellClicked = onCellClicked;
-      this.piece = piece;
-      this.createHtmlElement();
-    }
-    isEmpty() {
-      if (this.piece) {
-        return false;
-      }
-      return true;
-    }
-  
-    getPieceColor() {
-      return this.piece.color;
-    }
-  
-    onClick() {
-      if (this.piece) {
-        this.isSelected = !this.isSelected;
-        this.updateSelected();
-      }
-      if (this.onCellClicked) {
-        this.onCellClicked(this);
-      }
-    }
-  
-    createHtmlElement() {
-      this.htmlElement = document.createElement("td");
-      this.htmlElement.setAttribute(
-        `id`,
-        `${this.rowIndex}_${this.columnIndex}`
-      );
-      if (this.piece) {
-        this.htmlElement.appendChild(this.piece.htmlElement);
-      }
-      this.htmlElement.onclick = () => this.onClick();
-      return this.htmlElement;
-    }
-  
-    updateSelected() {
-      if (this.isSelected) {
-        this.htmlElement.style.backgroundColor = COLORS.SELECTED;
-      } else {
-        this.htmlElement.style.backgroundColor = COLORS.NOT_AVAILABLE;
-      }
-    }
-  
-    updateAvailable() {
-      if (this.isAvailable) {
-        //If cell available for move change background color to green
-        this.htmlElement.style.backgroundColor = COLORS.AVAILABLE;
-      } else {
-        this.htmlElement.style.backgroundColor = COLORS.NOT_AVAILABLE;
-      }
-    }
-  
-    isCellAvailableForMove(rowIndex, columnIndex, board) {
-      const cell = board.getCell(rowIndex, columnIndex);
-      if (cell.isEmpty()) {
-        return true;
-      }
+  constructor(rowIndex, columnIndex, isAvailable, piece, onCellClicked) {
+    this.rowIndex = rowIndex;
+    this.columnIndex = columnIndex;
+    this.isAvailable = isAvailable;
+    this.onCellClicked = onCellClicked;
+    this.piece = piece;
+    this.createHtmlElement();
+  }
+  isEmpty() {
+    if (this.piece) {
       return false;
     }
-  
-    isOtherPlayerCell(rowIndex, columnIndex, board) {
-      const cell = board.getCell(rowIndex, columnIndex);
-      if (cell.getPieceColor() !== this.getPieceColor()) {
-        return true;
-      }
-      return false;
+    return true;
+  }
+
+  getPieceColor() {
+    return this.piece.color;
+  }
+
+  onClick() {
+    if (this.piece) {
+      this.isSelected = !this.isSelected;
+      this.updateSelected();
+    }
+    if (this.onCellClicked) {
+      this.onCellClicked(this);
     }
   }
+
+  createHtmlElement() {
+    this.htmlElement = document.createElement("td");
+    this.htmlElement.setAttribute(
+      `id`,
+      `${this.rowIndex}_${this.columnIndex}`
+    );
+    if (this.piece) {
+      this.htmlElement.appendChild(this.piece.htmlElement);
+    }
+    this.htmlElement.onclick = () => this.onClick();
+    return this.htmlElement;
+  }
+
+  updateSelected() {
+    if (this.isSelected) {
+      this.htmlElement.style.backgroundColor = COLORS.SELECTED;
+    } else {
+      this.htmlElement.style.backgroundColor = COLORS.NOT_AVAILABLE;
+    }
+  }
+
+  updateAvailable() {
+    if (this.isAvailable) {
+      //If cell available for move change background color to green
+      this.htmlElement.style.backgroundColor = COLORS.AVAILABLE;
+    } else {
+      this.htmlElement.style.backgroundColor = COLORS.NOT_AVAILABLE;
+    }
+  }
+
+  isCellAvailableForMove(rowIndex, columnIndex, board) {
+    const cell = board.getCell(rowIndex, columnIndex);
+    if (cell.isEmpty()) {
+      return true;
+    }
+    return false;
+  }
+
+  isOtherPlayerCell(rowIndex, columnIndex, board) {
+    const cell = board.getCell(rowIndex, columnIndex);
+    if (cell.getPieceColor() !== this.getPieceColor()) {
+      return true;
+    }
+    return false;
+  }
+}
